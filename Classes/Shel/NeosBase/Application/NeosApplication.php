2008c0b65a808bdfb09ee91f120ed23b2e0a3943
Sebastian Helzle <sebastian@helzle.it>
2016-01-29 15:40:44 +0700

    [TASK] Added surf base app for Neos deployments

---
+++ Classes/Shel/NeosBase/Application/NeosApplication.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Shel\NeosBase\Application;
+
+/*                                                                        *
+ * This script belongs to the package "Shel.NeosBase".                    *
+ *                                                                        */
+
+use TYPO3\Surf\Application\TYPO3\Flow;
+use TYPO3\Surf\Domain\Model\Workflow;
+use TYPO3\Surf\Domain\Model\Deployment;
+
+/**
+ * Neos application template.
+ */
+class NeosApplication extends Flow
+{
+    /**
+     * @var bool
+     */
+    private $webopCacheResetActive;
+
+    /**
+     * @var string
+     */
+    private $baseUrl;
+
+    /**
+     * Constructor.
+     * @param string $name
+     * @param string $baseUrl
+     * @param bool $webopCacheResetActive
+     */
+    public function __construct($name = 'Neos', $baseUrl = '', $webopCacheResetActive = false)
+    {
+        parent::__construct($name);
+
+        $this->options = array_merge($this->options, [
+            'updateMethod' => null,
+            'keepReleases' => 3,
+            'packageMethod' => 'git',
+            'transferMethod' => 'rsync',
+        ]);
+        $this->webopCacheResetActive = $webopCacheResetActive;
+        $this->baseUrl = $baseUrl;
+    }
+
+    /**
+     * Register tasks for this application.
+     *
+     * @param Workflow $workflow
+     * @param Deployment $deployment
+     */
+    public function registerTasks(Workflow $workflow, Deployment $deployment)
+    {
+        parent::registerTasks($workflow, $deployment);
+
+        $this->setVersion('3.0');
+
+        // Define tasks
+        $workflow
+            ->defineTask('app:buildProject', 'typo3.surf:localShell',[
+                'command' => sprintf('cd {workspacePath} && %s', escapeshellcmd('./build.sh')),
+            ])
+            ->defineTask('app:optimizeAutoloader', 'typo3.surf:localShell',[
+                'command' => sprintf('cd {workspacePath} && %s dump-autoload --optimize', escapeshellcmd('composer')),
+            ])
+            ->defineTask('typo3.surf:shell:unsetResourceLinks', 'typo3.surf:shell',[
+                'command' => 'cd {releasePath} && rm -rf Web/_Resources/Persistent && rm -rf Web/_Resources/Static',
+            ])
+            ->defineTask('typo3.surf:shell:clearCaches', 'typo3.surf:shell',[
+                'command' => 'cd {releasePath} && FLOW_CONTEXT=' . $this->getContext() . ' ./flow flow:cache:flush'
+            ])
+        ;
+
+        // Add tasks to stages
+        $workflow
+            ->afterTask('typo3.surf:composer:localinstall', ['app:optimizeAutoloader', 'app:buildProject'])
+            ->beforeStage('finalize', ['typo3.surf:shell:unsetResourceLinks'], $this)
+            ->beforeStage('migrate', ['typo3.surf:shell:clearCaches'], $this)
+            ->afterStage('transfer', 'typo3.surf:typo3:flow:copyconfiguration')
+        ;
+
+        if ($this->webopCacheResetActive) {
+            $workflow
+                ->beforeStage('transfer', 'typo3.surf:php:webopcacheresetcreatescript')
+                ->afterStage('switch', 'typo3.surf:php:webopcacheresetexecute')
+            ;
+        }
+
+        // Configure deployment
+        $deployment
+            ->onInitialize(function () use ($workflow) {
+                $workflow
+                    ->setTaskOptions('typo3.surf:generic:createDirectories', [
+                        'directories' => [
+                            'shared/Data/Web/_Resources',
+                            'shared/Data/Session',
+                        ]
+                    ])
+                    ->setTaskOptions('typo3.surf:generic:createSymlinks', [
+                        'symlinks' => [
+                            'Data/Session' => '../../../shared/Data/Session/',
+                        ]
+                    ]);
+
+                if ($this->webopCacheResetActive) {
+                    $workflow->setTaskOptions('typo3.surf:php:webopcacheresetexecute', ['baseUrl' => $this->baseUrl]);
+                }
+            })
+        ;
+    }
+}
